5202a862db80196ca378bf136eb55b66591e8eb6
Grandmanouschka <82766780+Grandmanouschka@users.noreply.github.com>
2021-04-18 17:11:23 +0200

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+game.splash("Welcome to the one ", "and only...")
+game.splash("PET AUBERGINE GAME")
+
+scene.set_background_color(0)
--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+game.setDialogCursor(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 7 . . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . 7 7 7 7 . . . . . . 
+    . . . . 7 7 7 7 7 7 7 7 . . . . 
+    . . . . 7 7 c 7 c c 7 7 . . . . 
+    . . . 7 7 c c 7 7 c c 7 . . . . 
+    . . . 7 7 c c c 7 c c c c . . . 
+    . . . . c c f c c f c c c . . . 
+    . . . c c c f c c f c c c . . . 
+    . . . c c c c c c c c c c . . . 
+    . . . . c c f c c f c c c . . . 
+    . . . . c c c f f c c c c . . . 
+    . . . . c c c c c c c c . . . . 
+    . . . . . c c c c c c c . . . . 
+    . . . . . . c c c c . . . . . . 
+    `)
+game.splash("Welcome to the one ", "and only...")
+game.splash("PET AUBERGINE GAME")
+scene.setBackgroundColor(0)
