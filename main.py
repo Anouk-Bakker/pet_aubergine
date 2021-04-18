6ec237ef3da3a34ba78728ccc84a58141582673c
Grandmanouschka <82766780+Grandmanouschka@users.noreply.github.com>
2021-04-18 17:38:20 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,57 @@
+game.set_dialog_cursor(img("""
+    . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . 7 7 c 7 c c 7 7 . . . . 
+        . . . 7 7 c c 7 7 c c 7 . . . . 
+        . . . 7 7 c c c 7 c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . c c c f c c f c c c . . . 
+        . . . c c c c c c c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . . c c c f f c c c c . . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . c c c c . . . . . .
+"""))
 game.splash("Welcome to the one ", "and only...")
+game.set_dialog_cursor(img("""
+    . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . .
+"""))
 game.splash("PET AUBERGINE GAME")
-
-scene.set_background_color(0)
+game.set_dialog_cursor(img("""
+    . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . 7 7 c 7 c c 7 7 . . . . 
+        . . . 7 7 c c 7 7 c c 7 . . . . 
+        . . . 7 7 c c c 7 c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . c c c f c c f c c c . . . 
+        . . . c c c c c c c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . . c c c f f c c c c . . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . c c c c . . . . . .
+"""))
+game.show_long_text("Exciting isn't it?", DialogLayout.BOTTOM)
--- main.ts
+++ main.ts
@@ -1,21 +1,57 @@
 game.setDialogCursor(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . . 7 . . . . . . . . 
-    . . . . . . . 7 7 . . . . . . . 
-    . . . . . . 7 7 7 7 . . . . . . 
-    . . . . 7 7 7 7 7 7 7 7 . . . . 
-    . . . . 7 7 c 7 c c 7 7 . . . . 
-    . . . 7 7 c c 7 7 c c 7 . . . . 
-    . . . 7 7 c c c 7 c c c c . . . 
-    . . . . c c f c c f c c c . . . 
-    . . . c c c f c c f c c c . . . 
-    . . . c c c c c c c c c c . . . 
-    . . . . c c f c c f c c c . . . 
-    . . . . c c c f f c c c c . . . 
-    . . . . c c c c c c c c . . . . 
-    . . . . . c c c c c c c . . . . 
-    . . . . . . c c c c . . . . . . 
-    `)
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . 7 7 c 7 c c 7 7 . . . . 
+        . . . 7 7 c c 7 7 c c 7 . . . . 
+        . . . 7 7 c c c 7 c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . c c c f c c f c c c . . . 
+        . . . c c c c c c c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . . c c c f f c c c c . . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . c c c c . . . . . .
+`)
 game.splash("Welcome to the one ", "and only...")
+game.setDialogCursor(img`
+    . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . .
+`)
 game.splash("PET AUBERGINE GAME")
-scene.setBackgroundColor(0)
+game.setDialogCursor(img`
+    . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 7 . . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . 7 7 c 7 c c 7 7 . . . . 
+        . . . 7 7 c c 7 7 c c 7 . . . . 
+        . . . 7 7 c c c 7 c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . c c c f c c f c c c . . . 
+        . . . c c c c c c c c c c . . . 
+        . . . . c c f c c f c c c . . . 
+        . . . . c c c f f c c c c . . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . c c c c . . . . . .
+`)
+game.showLongText("Exciting isn't it?", DialogLayout.Bottom)
